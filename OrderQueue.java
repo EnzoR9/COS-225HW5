f0c34e10721f69b6619d588832cc205f3924ea8c
EnzoR9 <enzorussillo44@gmail.com>
2024-04-04 17:14:54 -0400

    orderqueue and ordertester

--- OrderQueue.java
+++ OrderQueue.java
@@ -1,6 +1,69 @@
-public class OrderQueue extends LinkedQueue
+public class OrderQueue<T> extends LinkedQueue<T>
 {
     public LinkedQueue<CustomerOrder> queue;
+
+    public int stock; 
+    public int queueSize;
+
+    public OrderQueue()
+    {
+        this.queue = new LinkedQueue<>();
+        this.stock = 0;
+        this.queueSize = 0;
+    }
+
+    public OrderQueue(int s)
+    {
+        this.queue = new LinkedQueue<>();
+        this.queueSize = 0;
+        this.stock = s;
+    }
+
+
+    public void addOrder(String name, String date, int quantity)
+    {
+        CustomerOrder order = new CustomerOrder(name, date, quantity);
+        queue.enqueue(order);
+        size++;
+
+    }
+
+    public void addStock(int addedStock)
+    {
+        stock += addedStock;
+    }
+
+    public void CompleteOrder()
+    {
+        if (queue.isEmpty() != true && stock < 0)
+        {
+            CustomerOrder order = queue.getFront();
+            order.shipProduct();
+            stock--;
+            if (order.quantity == 0)
+            {
+                queue.dequeue();
+                queueSize--;
+            }
+
+        }
+
+    }
+
+    public void sellOut()
+    {
+        while (stock > 0 && queue.isEmpty() == false)
+        {
+           CompleteOrder(); 
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        
+        return String.valueOf(queue.getFront().quantity);
+    }
 
 
     
--- OrderTester.java
+++ OrderTester.java
@@ -1,3 +1,31 @@
-public class OrderTester {
-    
+public class OrderTester 
+{
+    public static void main(String[] args)
+    {
+        OrderQueue customerQueue = new OrderQueue<>(14);
+        customerQueue.addOrder("Customer1", "date", 5);
+        customerQueue.addOrder("Customer1", "date", 5);
+        customerQueue.addOrder("Customer1", "date", 8);
+
+        
+        System.out.println(customerQueue);
+
+        // clear stock
+        customerQueue.sellOut();
+
+        
+        System.out.println(customerQueue);
+
+
+        // add 10 units to stock
+        customerQueue.addStock(10);
+        
+        // add three customer orders (3,3,5)
+        customerQueue.addOrder("Customer1", "date", 5);
+        customerQueue.addOrder("Customer1", "date", 5);
+        customerQueue.addOrder("Customer1", "date", 8);
+
+        // clear stock by filling orders with new stock of 8
+        // print queue 
+    }
 }
